ababae34e28ed0e3ca357ff98b043ed3ece6e90f
phueb <info@philhuebner.com>
2019-08-14 09:30:47 -0500

    fix submodules numbering

--- src/utils.py
+++ src/utils.py
@@ -25,27 +25,27 @@ def default_content(md_file_name):
         return "<h2>Content coming soon...</h2>" \
                "<h3><a href='/modules' style='text-decoration: none;'>&lArr; Return to Modules</a></h3>"
     else:
-        return "<h2>Content coming soon...</h2>" \
-               "<h3><a href='leaf=home' style='text-decoration: none;'>&lArr; Return to Module</a></h3>"
+        return "<h2>Content coming soon...</h2>"
 
 
 def load_content_and_submodules(static_path_name, branch_path_name, md_file_name):
 
     content_path = Path(static_path_name) / branch_path_name.lower()
 
-    # content
-    md_file = open_file(content_path, md_file_name)
-    if md_file is None:
-        print('Did not find {} in:\n{}'.format(md_file_name, content_path))
-        return default_content(md_file_name), None
-    md = md_file.read()
-    content = markdown.markdown(md, extensions=['extra', 'smarty'], output_format='html5')
-
+    # submodules
     sm_file_name = 'submodules.html'
     sm_file = open_file(content_path, sm_file_name)
     if sm_file is None:
         print('Did not find {} in:\n{}'.format(sm_file_name, content_path))
         return default_content(md_file_name), None
     submodules = sm_file.read()
+
+    # content
+    md_file = open_file(content_path, md_file_name)
+    if md_file is None:
+        print('Did not find {} in:\n{}'.format(md_file_name, content_path))
+        return default_content(md_file_name), submodules
+    md = md_file.read()
+    content = markdown.markdown(md, extensions=['extra', 'smarty'], output_format='html5')
 
     return content, submodules
